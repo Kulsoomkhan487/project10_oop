2205ef278558b543ae21ad53a81a31283c6ab319
Kulsoomkhan487 <kulsoomkhan.909@gmail.com>
2024-07-16 13:38:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (person) => {
+    do {
+        console.log("Welcome Guest!!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Whom would you like to talk to?",
+            choices: ["myself", "Student"]
+        });
+        if (ans.select === "myself") {
+            console.log("I am talking to myself");
+            console.log("My name's Kulsoom Khan, and I am from Karachi");
+        }
+        if (ans.select === "Student") {
+            const ansStudent = await inquirer.prompt({
+                name: "Student",
+                type: "input",
+                message: "Which Student do you wanna talk to?",
+            });
+            const student = persons.students.find(val => val.name === ansStudent.Student);
+            if (!student) {
+                const newStudent = new Student(ansStudent.Student);
+                persons.addStudent(newStudent);
+                console.log(`Hello I am ${newStudent.name}, and I am doing good..`);
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello I am ${student.name}, and I am fine............`);
+                console.log(persons.students);
+            }
+        }
+    } while (true);
+};
+programStart(persons);
